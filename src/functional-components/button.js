464519f65dcd00a457ea41a0cc3a17ebdbe785a9
nerellamanoj <nerellamanojkumar@gmail.com>
2024-04-20 21:06:49 +0530

    images and sign

---
+++ src/functional-components/button.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Button() {
+    var url="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+  return (
+   <>
+   <img src={url} width={100} height={100}/>
+        <button>singnup</button>
+
+   </>
+  )
+}
+
+export default Button
